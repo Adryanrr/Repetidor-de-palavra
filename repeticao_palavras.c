57dbf8f67445c2987cd2f88f302ab6dda8f89d6b
Adryan Ryan <adryanryan.s.g@hotmail.com>
2024-04-29 21:00:04 -0300

    Adicionando o repetidor de palavra

---
+++ repeticao_palavras.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int i, vezes;
+
+int main(){
+
+    printf("informe o numero de vezes que deseja repetir: \n");
+    scanf("%d", &vezes);
+
+    for (i = 0; i < vezes; i++)
+    {
+        printf("Palavra a ser repetida \n");
+    }
+
+}
